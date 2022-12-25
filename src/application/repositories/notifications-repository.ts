163768c60da2795e0a9fe662e225980bf07d34f4
Rosiele David <rosieleserpa@hotmail.com>
2022-12-25 20:16:31 -0300

    add caso de uso de cancelar notificação e seus testes

--- src/application/repositories/notifications-repository.ts
+++ src/application/repositories/notifications-repository.ts
@@ -3,4 +3,6 @@ import { Notification } from '../entities/notifications';
 // define os métodos da notificação, mas não implementa-os
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>;
+  abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract save(notification: Notification): Promise<void>;
 }
