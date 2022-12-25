748fe88732b1a0ba516d6caf8c80758f2fdb05ef
Rosiele David <rosieleserpa@hotmail.com>
2022-12-25 19:06:03 -0300

    configura typescript paths

--- test/repositories/in-memory-notifications.repository.ts
+++ test/repositories/in-memory-notifications.repository.ts
@@ -1,5 +1,5 @@
-import { Notification } from '../../src/application/entities/notifications';
-import { NotificationsRepository } from '../../src/application/repositories/notifications-repository';
+import { Notification } from '@application/entities/notifications';
+import { NotificationsRepository } from '@application/repositories/notifications-repository';
 
 // simula o banco de dados
 export class InMemoryNotificationsRepository
