77a80d3098342610fada57d2ffdad09d28aa8663
Rosiele David <rosieleserpa@hotmail.com>
2022-12-25 20:59:48 -0300

    add caso de uso de contagem de notificação por recipient e seu teste

---
+++ src/application/use-cases/count-recipient-notifications.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationsRepository } from '../repositories/notifications-repository';
+
+interface CountRecipientNotificationsRequest {
+  recipientId: string;
+}
+
+interface CountRecipientNotificationsResponse {
+  count: number;
+}
+
+@Injectable() // necessário utilizar para que possa ser injetado no constructor do notification controller (algo do nestjs)
+export class CountRecipientNotifications {
+  constructor(private notificationRepository: NotificationsRepository) {}
+
+  async execute(
+    request: CountRecipientNotificationsRequest,
+  ): Promise<CountRecipientNotificationsResponse> {
+    const { recipientId } = request;
+
+    const count = await this.notificationRepository.countManyByRecipientId(
+      recipientId,
+    );
+
+    return {
+      count,
+    };
+  }
+}
