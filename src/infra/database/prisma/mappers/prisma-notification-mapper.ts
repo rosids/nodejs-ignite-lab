87a917d6b9548e9146fd3e9ff3f6b7c51e0b78fb
Rosiele David <rosieleserpa@hotmail.com>
2022-12-25 21:48:10 -0300

    add mapper de conversão da notificação do prisma para camada de domínio da aplicação

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,6 +1,10 @@
+import { Notification as RawNotification } from '@prisma/client';
 import { Notification } from '@application/entities/notifications';
+import { Content } from '@application/entities/content';
 
 export class PrismaNotificationMapper {
+  // converte a entidade de notification da pasta application/entities para o prisma
+  // camada de persistência
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
@@ -10,5 +14,21 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  // converte a entidade de notification do prisma para notification da pasta application/entities
+  // camada de domínio
+  static toDomain(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
