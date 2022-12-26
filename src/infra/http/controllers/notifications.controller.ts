18d23ea08f594d69689ee08cea64a7eada1e8e4e
Rosiele David <rosieleserpa@hotmail.com>
2022-12-25 22:33:44 -0300

    add rota de cancelar notificação

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
 import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) {}
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+  ) {}
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id,
+    });
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationBody) {
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
 
 @Module({
   // por usar o SendNotification no providers e a classe do SendNotification necessitar do NotificationsRepository poderia utilizar a mesma estratégia do DatabaseModule usando o provide ou simplesmente adicionar o DatabaseModule no imports
   imports: [DatabaseModule],
   controllers: [NotificationsController],
   // para que possa ser feita a injeção de dependência no controller é necessário adicioná-lo no provider
-  providers: [SendNotification],
+  providers: [SendNotification, CancelNotification],
 })
 export class HttpModule {}
